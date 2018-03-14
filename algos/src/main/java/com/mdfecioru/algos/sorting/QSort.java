f61d0aebfda8f7293e80c8a24d571579273f7605
Mihai Fecioru <mdfecioru@gmail.com>
2018-03-14 14:24:47 +0200

    Final version of QSort

--- algos/src/main/java/com/mdfecioru/algos/sorting/QSort.java
+++ algos/src/main/java/com/mdfecioru/algos/sorting/QSort.java
@@ -4,13 +4,80 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class QSort {
-  private ArrayList al;
+  private ArrayList<Integer> al;
+  private int comparisions;
 
   public QSort(Collection<Integer> c) {
     al = new ArrayList<Integer>(c);
+    comparisions = 0;
   }
   
-  ArrayList sort() {
+  int choosePivot(int start, int end) {
+    int v1 = al.get(start);
+    int v2 = al.get( start + (end - start - 1) / 2);
+    int v3 = al.get(end-1);
+
+    int[] v = {v1, v2, v3};
+
+    for (int i=0; i<2; i++) {
+      for (int j=i+1; j<3; j++) {
+        if (v[i] > v[j]) {
+          int temp = v[i];
+          v[i] = v[j];
+          v[j] = temp;
+        }
+      }
+    }
+
+    if (v[1] == v1) return start;
+    if (v[1] == v2) return start + (end - start - 1) / 2;
+    return end - 1;
+  }
+
+  private void swap(int i, int j) {
+    Integer temp = al.get(i);
+    al.set(i, al.get(j));
+    al.set(j, temp);
+  }
+
+  private int partitionAroundPivot(int start, int end, int pivotPosition) {
+    swap(start, pivotPosition);
+    int i, j;
+
+    i = start+1;
+    Integer pivot = al.get(start);
+    for (j = start+1; j < end; j++) {
+      if (al.get(j) < pivot) {
+        swap(i, j);
+        i++;
+      }
+    }
+    swap(i-1, start);
+
+    return i-1;
+  }
+
+
+  private void _sort(int start, int end) {
+    if (end - start <= 1) return;
+
+    int pivotPosition = choosePivot(start, end);
+    pivotPosition = partitionAroundPivot(start, end, pivotPosition);
+    
+    if (pivotPosition - start >= 1) comparisions = comparisions + (pivotPosition - start - 1);
+    _sort(start, pivotPosition);
+   
+    if (end - (pivotPosition+1) >= 1) comparisions = comparisions + (end - (pivotPosition+1) - 1);
+    _sort(pivotPosition+1, end);
+  }
+
+  public int getComparisions() {
+    return comparisions;
+  }
+
+  public ArrayList sort() {
+    comparisions = comparisions + (al.size() - 1);
+    _sort(0, al.size());
     return new ArrayList<Integer>(al);
   }
 
--- algos/src/test/java/com/mdfecioru/algos/sorting/QSortTest.java
+++ algos/src/test/java/com/mdfecioru/algos/sorting/QSortTest.java
@@ -10,23 +10,70 @@ import static org.junit.Assert.*;
 public class QSortTest {
 
     @Test
-    public void checkSortFromFile() {
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        //ReadFromFile.readIntegers(this.getClass().getResourceAsStream("../../../../QSortTest1.txt"), al);
-        al.add(1);
-        al.add(2);
-        al.add(3);
-        al.add(3);
-        al.add(4);
-        al.add(6);
-        al.add(23);
-        al.add(42);
-        al.add(54);
+    public void checkSortFromVector() {
+        ArrayList<Integer> al = new ArrayList<Integer>();        
+
+        al.add(4); al.add(3); al.add(3); al.add(1); al.add(23); 
+        al.add(54);al.add(2); al.add(6);al.add(42);
 
         QSort qs = new QSort(al);
         al = qs.sort();
+    
         assertArrayEquals(new int[]{1, 2, 3, 3, 4, 6, 23, 42, 54},
                           al.stream().mapToInt(i -> i).toArray());
+    }
+
+    @Test
+    public void checkSortFromFile() {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        int i;
+        ReadFromFile.readIntegers(this.getClass().getResourceAsStream("../../../../QSortTest1.txt"), al);
+
+        QSort qs = new QSort(al);
+        al = qs.sort();
+        
+        boolean testPassed = true;
+        for (i=0; i<al.size()-1; i++) {
+          if (al.get(i) > al.get(i+1)) {
+            testPassed = false;
+            break;
+          }
+        }
+
+        System.out.println("Comparisions: " + qs.getComparisions());
+
+        assertTrue("Test failed at index " + i, testPassed);
+    }
+
+    @Test
+    public void checkchoosePivotEven() {
+      ArrayList<Integer> al = new ArrayList<Integer>();        
+
+        al.add(8); al.add(2); al.add(4); al.add(5); al.add(7); al.add(1); 
+
+        QSort qs = new QSort(al);
+        assertEquals(qs.choosePivot(0, al.size()), 2);        
+    }
+
+    @Test
+    public void checkchoosePivotOdd() {
+      ArrayList<Integer> al = new ArrayList<Integer>();        
+
+        al.add(8); al.add(2); al.add(4); al.add(3); al.add(7); al.add(1); al.add(5);
+
+        QSort qs = new QSort(al);
+        assertEquals(qs.choosePivot(0, al.size()), 6);        
+    }
+
+    @Test
+    public void checkchoosePivotInsideArray() {
+      ArrayList<Integer> al = new ArrayList<Integer>();        
+
+        al.add(8); al.add(12); al.add(4); al.add(3); al.add(7); al.add(10); al.add(5);
+        al.add(1); al.add(17); al.add(1); al.add(2);
+
+        QSort qs = new QSort(al);
+        assertEquals(qs.choosePivot(2, 9), 5);        
     }
 
 }
